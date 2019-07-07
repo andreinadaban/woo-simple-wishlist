7335df00ecc0878c4c5785c60387ffac2b83ff3a
Andrei Nadaban <contact@andreinadaban.ro>
2019-07-07 18:17:08 +0300

    Thumb in separate td and more action hooks

--- templates/wishlist.php
+++ templates/wishlist.php
@@ -19,11 +19,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <table class="wcsw-table">
 
-    <thead>
+    <thead class="wcsw-thead">
 
         <tr>
 
-            <?php do_action( 'wcsw_before_th_title' ); ?>
+	        <?php do_action( 'wcsw_before_th_thumb' ); ?>
+
+            <th></th>
+
+            <?php do_action( 'wcsw_after_th_thumb' ); ?>
 
             <th><?php _e( 'Product', 'wcsw' ); ?></th>
 
@@ -35,39 +39,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     </thead>
 
-    <tbody>
+    <tbody class="wcsw-tbody">
+
+        <?php do_action( 'wcsw_after_tbody_start' ); ?>
 
         <?php foreach ( $products as $product_id => $product_data ) { ?>
 
             <tr>
 
-                <?php do_action( 'wcsw_before_td_title' ); ?>
+	            <?php do_action( 'wcsw_before_td_thumb' ); ?>
 
-                <td class="wcsw-td">
+                <td class="wcsw-thumb">
+
+	                <?php if ( get_post( $product_id ) && get_post_status( $product_id ) === 'publish' ) { ?>
+
+                        <a href="<?php echo get_the_permalink( $product_id ); ?>">
+
+                            <img src="<?php echo get_the_post_thumbnail_url( $product_id, 'woocommerce_gallery_thumbnail' ) ?>" alt="<?php echo get_the_title( $product_id ); ?>">
+
+                        </a>
+
+	                <?php } else { ?>
+
+                        <div><img src="<?php echo wc_placeholder_img_src(); ?>" alt=""></div>
+
+	                <?php } ?>
+
+                </td>
+
+                <?php do_action( 'wcsw_after_td_thumb' ); ?>
+
+                <td class="wcsw-title">
 
                     <?php if ( get_post( $product_id ) && get_post_status( $product_id ) === 'publish' ) { ?>
 
-                        <a href="<?php echo get_the_permalink( $product_id ); ?>" class="wcsw-title">
+                        <a href="<?php echo get_the_permalink( $product_id ); ?>">
 
-                            <img src="<?php echo get_the_post_thumbnail_url( $product_id, 'woocommerce_gallery_thumbnail' ) ?>" alt="<?php echo get_the_title( $product_id ); ?>" class="wcsw-thumb">
-
-                            <span class="wcsw-title-text"><?php echo get_the_title( $product_id ); ?></span>
+                            <?php echo get_the_title( $product_id ); ?>
 
                         </a>
 
                     <?php } else { ?>
 
-                        <div class="wcsw-title">
-
-                            <img src="<?php echo wc_placeholder_img_src(); ?>" alt="" class="wcsw-thumb">
-
-                            <span class="wcsw-title-text">
-
-                                <?php echo $product_data['title'] . ' (' . __( 'no longer available', 'wcsw' ) . ')'; ?>
-
-                            </span>
-
-                        </div>
+                        <div><?php echo $product_data['title'] . ' (' . __( 'no longer available', 'wcsw' ) . ')'; ?></div>
 
                     <?php } ?>
 
@@ -75,17 +89,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
                 <?php do_action( 'wcsw_after_td_title' ); ?>
 
-                <td class="wcsw-td wcsw-actions">
+                <td class="wcsw-actions">
 
                     <a href="?wcsw-remove=<?php echo $product_id; ?>&nonce-token=<?php echo wp_create_nonce( 'wcsw_remove_from_wishlist_' . $product_id ) ?>" class="wcsw-button wcsw-button-ajax wcsw-button-remove remove" title="<?php _e( 'Remove from wishlist', 'wcsw' ); ?>"></a>
 
                 </td>
 
+	            <?php do_action( 'wcsw_after_td_actions' ); ?>
+
             </tr>
 
         <?php } ?>
 
+        <?php do_action( 'wcsw_before_tbody_end' ); ?>
+
     </tbody>
+
+	<?php do_action( 'wcsw_after_tbody' ); ?>
 
 </table>
 
