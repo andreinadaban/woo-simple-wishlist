82d9f1beeaf2b8e4c8950dd7e9a5f752d2317230
Andrei Nadaban <contact@andreinadaban.ro>
2019-07-07 17:57:18 +0300

    Indentation

--- templates/wishlist.php
+++ templates/wishlist.php
@@ -21,69 +21,69 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <thead>
 
-    <tr>
+        <tr>
 
-        <?php do_action( 'wcsw_before_th_title' ); ?>
+            <?php do_action( 'wcsw_before_th_title' ); ?>
 
-        <th><?php _e( 'Product', 'wcsw' ); ?></th>
+            <th><?php _e( 'Product', 'wcsw' ); ?></th>
 
-        <?php do_action( 'wcsw_after_th_title' ); ?>
+            <?php do_action( 'wcsw_after_th_title' ); ?>
 
-        <th></th>
+            <th></th>
 
-    </tr>
+        </tr>
 
     </thead>
 
     <tbody>
 
-    <?php foreach ( $products as $product_id => $product_data ) { ?>
+        <?php foreach ( $products as $product_id => $product_data ) { ?>
 
-        <tr>
+            <tr>
 
-            <?php do_action( 'wcsw_before_td_title' ); ?>
+                <?php do_action( 'wcsw_before_td_title' ); ?>
 
-            <td class="wcsw-td">
+                <td class="wcsw-td">
 
-                <?php if ( get_post( $product_id ) && get_post_status( $product_id ) === 'publish' ) { ?>
+                    <?php if ( get_post( $product_id ) && get_post_status( $product_id ) === 'publish' ) { ?>
 
-                    <a href="<?php echo get_the_permalink( $product_id ); ?>" class="wcsw-title">
+                        <a href="<?php echo get_the_permalink( $product_id ); ?>" class="wcsw-title">
 
-                        <img src="<?php echo get_the_post_thumbnail_url( $product_id, 'woocommerce_gallery_thumbnail' ) ?>" alt="<?php echo get_the_title( $product_id ); ?>" class="wcsw-thumb">
+                            <img src="<?php echo get_the_post_thumbnail_url( $product_id, 'woocommerce_gallery_thumbnail' ) ?>" alt="<?php echo get_the_title( $product_id ); ?>" class="wcsw-thumb">
 
-                        <span class="wcsw-title-text"><?php echo get_the_title( $product_id ); ?></span>
+                            <span class="wcsw-title-text"><?php echo get_the_title( $product_id ); ?></span>
 
-                    </a>
+                        </a>
 
-                <?php } else { ?>
+                    <?php } else { ?>
 
-                    <div class="wcsw-title">
+                        <div class="wcsw-title">
 
-                        <img src="<?php echo wc_placeholder_img_src(); ?>" alt="" class="wcsw-thumb">
+                            <img src="<?php echo wc_placeholder_img_src(); ?>" alt="" class="wcsw-thumb">
 
-                        <span class="wcsw-title-text">
+                            <span class="wcsw-title-text">
 
-                            <?php echo $product_data['title'] . ' (' . __( 'no longer available', 'wcsw' ) . ')'; ?>
+                                <?php echo $product_data['title'] . ' (' . __( 'no longer available', 'wcsw' ) . ')'; ?>
 
-                        </span>
+                            </span>
 
-                    </div>
+                        </div>
 
-                <?php } ?>
+                    <?php } ?>
 
-            </td>
+                </td>
 
-            <?php do_action( 'wcsw_after_td_title' ); ?>
+                <?php do_action( 'wcsw_after_td_title' ); ?>
 
-            <td class="wcsw-td wcsw-actions">
+                <td class="wcsw-td wcsw-actions">
 
-                <a href="?wcsw-remove=<?php echo $product_id; ?>&nonce-token=<?php echo wp_create_nonce( 'wcsw_remove_from_wishlist_' . $product_id ) ?>" class="wcsw-button wcsw-button-ajax wcsw-button-remove remove" title="<?php _e( 'Remove from wishlist', 'wcsw' ); ?>"></a>
+                    <a href="?wcsw-remove=<?php echo $product_id; ?>&nonce-token=<?php echo wp_create_nonce( 'wcsw_remove_from_wishlist_' . $product_id ) ?>" class="wcsw-button wcsw-button-ajax wcsw-button-remove remove" title="<?php _e( 'Remove from wishlist', 'wcsw' ); ?>"></a>
 
-            </td>
+                </td>
 
-        </tr>
+            </tr>
 
-    <?php } ?>
+        <?php } ?>
 
     </tbody>
 
