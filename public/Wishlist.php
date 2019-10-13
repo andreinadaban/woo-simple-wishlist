72cf5ae2d15b5b19f613082170db6fda8008f2f2
Andrei Nadaban <contact@andreinadaban.ro>
2019-10-13 12:51:18 +0300

    Removed unused JS variables

--- public/Wishlist.php
+++ public/Wishlist.php
@@ -487,24 +487,11 @@ class Wishlist {
 		}
 
 		$ajax_url = get_admin_url() . 'admin-ajax.php';
-		$empty_wishlist_notice = $this->get_empty_wishlist_notice();
-
-		$is_account_page = is_account_page() ? 'true' : 'false';
-		$is_single_page  = is_singular( 'product' ) ? 'true' : 'false';
-		$is_product_page = ( is_shop() ||
-		                     is_product_category() ||
-		                     is_product_tag() ||
-		                     is_singular( 'product' ) ) ? 'true' : 'false';
 
 		echo <<<EOT
 			<script>
 			
 				var ajaxURL = '{$ajax_url}';
-				var emptyWishlistNotice = '{$empty_wishlist_notice}';
-				
-				var isAccountPage = {$is_account_page};
-				var isProductPage = {$is_product_page};
-				var isSinglePage  = {$is_single_page};
 			
 			</script>
 EOT;
